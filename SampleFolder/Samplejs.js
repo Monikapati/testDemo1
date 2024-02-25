22931f00a70b3e54b73d641b5bfa2d719db112fa
pmonika <pmonika@galaxe.com>
2024-02-25 11:22:34 +0530

    updated changes

---
+++ SampleFolder/Samplejs.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { HelloWorld, Welcome } from './HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+          <h1>Learn React</h1>
+          <HelloWorld/>
+    <Welcome />
+    </div>
+    
+  );
+}
+
+export default App;
